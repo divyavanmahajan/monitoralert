014d942a16d7a1a33ed1ecfe8729a27bfed6f65b
ndtaylor2 <ndtaylor2@gmail.com>
2016-05-17 07:52:54 -0400

    Switched from the date range test mode to evaluating the last ten records, commented out the console.log items and cleaned up the formating

--- index.js
+++ index.js
@@ -25,9 +25,9 @@ monitoralert.initialize = function(dbconfig) {
 var counter = 0;
 var observations = {};
 function setupListener() {
-  //var query = monitoralert.util.baseRef.orderByKey().limitToLast(10);
-  var query = monitoralert.util.baseRef.orderByKey().startAt('2016-05-11T01:40:00').endAt('2016-05-11T03:15:00');
-	// Use second one for testing on a particular date range
+  var query = monitoralert.util.baseRef.orderByKey().limitToLast(10);
+  // Use for testing a particular date range
+  // var query = monitoralert.util.baseRef.orderByKey().startAt('2016-05-11T01:40:00').endAt('2016-05-11T03:15:00');
   query.on("child_added",addObservation);
   query.on("child_changed",addObservation);
   query.on("child_removed",removeObservation);
@@ -39,6 +39,7 @@ function removeObservation(dataSnapshot) {
             console.info("remove", dataSnapshot.key());//,JSON.stringify(dataSnapshot.val()));
         }
 }
+
 function addObservation(data) {
 	if (data.exists()) {
 	    var key=data.key();
@@ -46,27 +47,24 @@ function addObservation(data) {
 	    if (key[0]!='2') return;
 	    observations[data.key()]=data.val();
 	    // Get the record
-	    // Logic here on the counter
 	    if (val.ServiceContract_missed >0) {console.info("add",data.key(),val.ServiceContract_missed,val.ServiceContract_latency); //JSON.stringify(data.val()));
 		}
-    // Inject counter logic here
-    var errors = val.ServiceContract_missed;
-
-       if (errors > 0){
-            console.log('yes there is an error');
+      //evaluate if an alert needs to be sent
+      var errors = val.ServiceContract_missed;
+      if (errors > 0){
+            //console.log('yes there is an error');
             //and the counter is at 2
             if (counter == 2){
-                console.log('yes the counter is at 2');
+                //console.log('yes the counter is at 2');
                 //send an alert
-                var message = "3 in a row";
-                console.error("SENT ALERT");
                 monitoralert.sendalert.makecall();
+                console.error("Phone call alert sent");
                 //reset counter to zero
                 counter = 0;
             }
             //otherwise add 1 to the counter
             else {
-                console.log('counter not at 2, increment the counter');
+                //console.log('counter not at 2, increment the counter');
                 counter = counter +1;
             }
         }
@@ -82,6 +80,5 @@ function onCtrlC() {
     console.info("\nGracefully shutting down from SIGINT (Ctrl+C)");
     setTimeout(process.exit, 2000); // Wait 2 seconds and exit. This is needed for Firebase to exit.
 }
-
 
 monitoralert.initialize(monitoralert.config);
